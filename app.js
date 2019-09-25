9f8706fe9679fede16ac6842d219a7cd2806304c
Maciej Kaiser <maciejkaiser@hotmail.com>
2019-09-25 19:54:23 +0200

    implementation of openweather api with coords data from geolocation

--- app.js
+++ app.js
@@ -1,3 +1,69 @@
 window.addEventListener("load", () => {
-     console.log("Loaded");
+
+    const apiKey = ""; //secret
+    let api = "";
+
+    let temperatureWrapper = document.querySelector(".temperature")
+    let temperatureDegree = document.querySelector(".temperature-degree");
+    let degreeSection = document.querySelector(".degree-section");
+    let temperatureSpan = document.querySelector(".temperature span")
+    let locationTimezone = document.querySelector(".location-timezone");
+    let weatherDescription = document.querySelector(".temperature-description");
+
+    let coordsLatitude = 0;
+    let coordsLongitude = 0;
+    let cityName = "";
+    let temperatureValue = 0;
+    let humidityValue = 0;
+
+    
+
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(position => {
+
+            coordsLatitude = position.coords.latitude;
+            coordsLongitude = position.coords.longitude;
+
+            api = `https://api.openweathermap.org/data/2.5/weather?lat=${coordsLatitude}&lon=${coordsLongitude}&units=metric&appid=${apiKey}`;
+
+            fetch(api).then(response => {
+                return response.json();
+
+            }).then(data => {
+                console.log(data);
+
+                const {
+                    temp,
+                    humidity,
+                    pressure
+                } = data.main;
+                const {
+                    description
+                } = data.weather[0];
+                const name = data.name;
+
+                cityName = name;
+                temperatureValue = temp;
+                humidityValue = humidity;
+
+                temperatureDegree.textContent = temperatureValue;
+                locationTimezone.textContent = cityName;
+                weatherDescription.textContent = description;
+
+            })
+
+        }, function (e) {
+            showAlert(document.body, "Geolocation permission is denied");
+        });
+    } else {
+        showAlert(document.body, "Geolocation is not supported by this browser");
+    }
+
+    function showAlert(element, message) {
+        var alert = document.createElement('div');
+        alert.className = "alert";
+        alert.innerText = message;
+        element.appendChild(alert);
+    }
 });
